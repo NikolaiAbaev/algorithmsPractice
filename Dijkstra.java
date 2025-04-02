81c4b9e90893fd199c2ed250d8cbd8d751acb007
Nikolai Abaev <87283130+NikolaiAbaev@users.noreply.github.com>
2025-04-02 13:14:55 -0400

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -2,34 +2,73 @@ import java.util.*;
 
 public class Dijkstra {
 
-    public Dijkstra(HashMap<String, HashMap<String, Integer>> graph,
-                    HashMap<String, Integer> costs,
-                    HashMap<String, String> parents) {
+    public Dijkstra(HashMap<String, HashMap<String, Integer>> graph, String start, String finish) {
 
+        // Creating a Cost Table and a Parent Table
+        HashMap<String, Integer> costs = new HashMap<>();
+        HashMap<String, String> parents = new HashMap<>();
+
+        for (Map.Entry<String, HashMap<String, Integer>> entry : graph.entrySet()) {
+            costs.put(entry.getKey(), Integer.MAX_VALUE);
+        }
+
+        for (Map.Entry<String, Integer> entry : graph.get(start).entrySet()) {
+            costs.put(entry.getKey(), entry.getValue());
+            parents.put(entry.getKey(), start);
+        }
+        costs.put(start, 0);
+        // Finding The Fastest Path
         HashSet<String> visited = new HashSet<>();
         String edge = findLowestEdge(costs, visited);
-        while (true) {
-            // the algorithm goes here.
-            try {
-                String edge = findLowestEdge(costs, visited);
-            } catch (IllegalArgumentException e) {
-                break;
+        while (edge != null) {
+
+            int cost = costs.get(edge);
+            for (Map.Entry<String, Integer> entry : graph.get(edge).entrySet()) {
+                String tempVertex = entry.getKey();
+                int tempWeight = entry.getValue();
+                int sumWeight = cost + tempWeight;
+
+                if (costs.get(tempVertex) > sumWeight) {
+                    costs.put(tempVertex, sumWeight);
+                    parents.put(tempVertex, edge);
+                }
+            }
+            visited.add(edge);
+            edge = findLowestEdge(costs, visited);
+        }
+
+        List<String> path = new ArrayList<>();
+        String traverser = finish;
+        while (traverser != null) {
+            path.add(traverser);
+            traverser = parents.get(traverser);
+        }
+        Collections.reverse(path);
+        System.out.printf("The path is '%s' and its weight is %d.\n", String.join(" -> ", path), costs.get(finish));
+    }
+
+
+    public static String pathCreator(String s) {
+        StringBuilder res = new StringBuilder();
+        for (int i = s.length() - 1; i > -1; i--) {
+            res.append(s.charAt(i));
+            if (i != 0) {
+                res.append(" -> ");
             }
         }
+        return res.toString();
     }
 
     public static String findLowestEdge(HashMap<String, Integer> vertex, HashSet<String> visited) {
         int min = Integer.MAX_VALUE;
-        String minEntry;
+        String minEntry = null;
         for (Map.Entry<String, Integer> entry : vertex.entrySet()) {
             if (!visited.contains(entry.getKey()) && entry.getValue() < min) {
                 minEntry = entry.getKey();
                 min = entry.getValue();
             }
         }
-        if (minEntry == null) {
-            throw new IllegalArgumentException("The input vertex map is empty.");
-        }
+
         return minEntry;
     }
 
@@ -49,6 +88,7 @@ public class Dijkstra {
         neighborsB.put("C", 8);
         neighborsB.put("E", 2);
         neighborsB.put("D", 4);
+        neighborsB.put("A", 5);
         graph.put("B", neighborsB);
 
         HashMap<String, Integer> neighborsC = new HashMap<>();
@@ -64,7 +104,7 @@ public class Dijkstra {
         graph.put("D", neighborsD);
 
         HashMap<String, Integer> neighborsE = new HashMap<>();
-        neighborsE.put("B", 4);
+        neighborsE.put("B", 2);
         neighborsE.put("C", 7);
         neighborsE.put("D", 6);
         neighborsE.put("F", 1);
@@ -75,15 +115,6 @@ public class Dijkstra {
         neighborsF.put("E", 1);
         graph.put("F", neighborsF);
 
-        //set up the Cost Table
-        costs.put("B", 5);
-        costs.put("C", 2);
-        costs.put("D", Integer.MAX_VALUE);
-        costs.put("E", Integer.MAX_VALUE);
-        costs.put("F", Integer.MAX_VALUE);
-
-        // set up Parents
-        parents.put("B", "A");
-        parents.put("C", "A");
+        new Dijkstra(graph, "A", "D");
     }
 }
