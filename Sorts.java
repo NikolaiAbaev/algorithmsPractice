33f3843fc02666eb1f1ba9c90c1197436343f44c
Nikolai Abaev <87283130+NikolaiAbaev@users.noreply.github.com>
2025-06-11 23:02:47 -0400

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,16 +1,37 @@
 public class Sorts {
 
-  public static void selectionSort (int[] array){
-    int n = array.length;
-    int temp;
-    for (int i = 0; i < n - 1; i++){
-      for (int j = i + 1; j < n; j++){
-        if (array[i] > array[j]){
-          temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
+    public static void selectionSortUpdate (int[] array) {
+        int n = array.length;
+        int temp;
+        int index;
+
+        for (int i = 0; i < n; i++) {
+            temp = array[i];
+            index = i;
+            for (int j = i; j < n; j++) {
+                if (temp > array[j]) {
+                    temp = array[j];
+                    index = j;
+                }
+            }
+            array[index] = array[i];
+            array[i] = temp;
         }
-      }
     }
-  }
+
+    public static void selectionSortOriginal (int[] array) {
+        int n = array.length;
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+
 }
